c2b3eed081803294def99c4fb1ba9a62c32e7404
Lucas Tomic Benitez <65186233+lucastomic@users.noreply.github.com>
2024-10-09 19:55:57 +0200

    JUnit added

---
+++ src/test/java/space_invaders/sprites/AlienTest.java
@@ -0,0 +1,7 @@
+package space_invaders.sprites;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlienTest {
+
+}
---
+++ src/test/java/space_invaders/sprites/PlayerTest.java
@@ -0,0 +1,20 @@
+package space_invaders.sprites;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void act() {
+    }
+
+    @Test
+    void keyPressed() {
+    }
+
+    @Test
+    void keyReleased() {
+    }
+}
