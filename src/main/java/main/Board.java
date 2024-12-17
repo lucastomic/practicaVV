269d04961a901dd4ab8d0c9827a39dd46a481e4f
Lucas Tomic Benitez <65186233+lucastomic@users.noreply.github.com>
2024-12-17 22:12:35 +0100

    Board integration tests

--- src/main/java/main/Board.java
+++ src/main/java/main/Board.java
@@ -56,23 +56,42 @@ public class Board extends JPanel {
      * */
     public Board() {
         generator = new Random();
-        initBoard();
+        initBoard(true);
         gameInit();
     }
 
     /**
+     * Inicializa el tablero y comienza la partida dependiendo del parametro withTimer
+     * @param withTimer boolean que solo es true en caso de que se quiera iniciar el timer automáticamente
+     */
+    public Board(boolean withTimer) {
+        generator = new Random();
+        initBoard(withTimer);
+        gameInit();
+    }
+    /**
      * Inicializa un nuevo tablero con las dimensiones predefinidas, le asigna un fondo de color negro, inicializa el contador de juego e inicia la partida.
      * NO ES NECESARIO PROBAR ESTE MÉTODO MEDIANTE PRUEBAS UNITARIAS
      * */
-    private void initBoard() {
+    private void initBoard(boolean withTimer) {
+        addKeyListener(new TAdapter());
+        setFocusable(true);
+        d = new Dimension(Commons.BOARD_WIDTH, Commons.BOARD_HEIGHT);
+        setBackground(Color.black);
+        if(withTimer) {
+            timer = new Timer(Commons.DELAY, new GameCycle());
+            timer.start();
+        }
+        gameInit();
+    }
+
+
+    private void initBoardWithoutTimer() {
 
         addKeyListener(new TAdapter());
         setFocusable(true);
         d = new Dimension(Commons.BOARD_WIDTH, Commons.BOARD_HEIGHT);
         setBackground(Color.black);
-
-        timer = new Timer(Commons.DELAY, new GameCycle());
-        timer.start();
 
         gameInit();
     }
@@ -334,6 +353,7 @@ public class Board extends JPanel {
                         inGame = false;
                         message = "Invasion!";
                     }
+                    System.out.println(direction);
                     alien.act(direction);
                 }
             }
--- src/test/java/integration/BoardIntegrationTest.java
+++ src/test/java/integration/BoardIntegrationTest.java
@@ -1,29 +1,41 @@
 package integration;
 import main.Board;
+import main.Commons;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import space_invaders.sprites.Alien;
 import space_invaders.sprites.Player;
+import space_invaders.sprites.Shot;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.Timer;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class BoardIntegrationTest {
-
     @Test
     public void testUpdateWithAllMocks() {
-        // Arrange
-        Board board = Mockito.spy(new Board());
-        board.setPlayer(mock(Player.class));
+        Board board = Mockito.spy(new Board(false));
+
+        //Mock del player
+        Player player = mock(Player.class);
+        when(player.isVisible()).thenReturn(true);
+        when(player.getX()).thenReturn(Commons.BOARD_WIDTH / 2 - Commons.PLAYER_WIDTH);
+        when(player.getY()).thenReturn(Commons.GROUND - Commons.PLAYER_HEIGHT);
+        board.setPlayer(player);
+
+        //Mock de aliens
+        board.setAliens(createMockAliens());
 
         // Mockear métodos individuales
-        doNothing().when(board).update_shots();
+        doAnswer(invocation -> {
+            board.getShot().die();
+            return null;
+        }).when(board).update_shots();
         doNothing().when(board).update_aliens();
         doNothing().when(board).update_bomb();
-
 
         // Act
         board.update();
@@ -32,12 +44,50 @@ public class BoardIntegrationTest {
         verify(board, times(1)).update_shots();
         verify(board, times(1)).update_aliens();
         verify(board, times(1)).update_bomb();
+        verify(player, times(1)).act();
+
+        // Assert: Verificar funcionamiento esperado
+        generalAsserts(board);
     }
+    @Test
+    public void testUpdateWithRealPlayer() {
+        // Arrange
+        Board board = Mockito.spy(new Board(false));
+        // Utilizamos un player real
+        board.setPlayer(new Player());
+
+        //Mock de aliens
+        board.setAliens(createMockAliens());
+
+        // Mockear métodos individuales
+        doAnswer(invocation -> {
+            board.getShot().die();
+            return null;
+        }).when(board).update_shots();
+        doNothing().when(board).update_aliens();
+        doNothing().when(board).update_bomb();
+
+        // Act
+        board.update();
+
+        // Assert
+        verify(board, times(1)).update_shots();
+        verify(board, times(1)).update_aliens();
+        verify(board, times(1)).update_bomb();
+
+
+        // Assert: Verificar funcionamiento esperado
+        generalAsserts(board);
+    }
+
     @Test
     public void testUpdateWithRealUpdateShots() {
         // Arrange
-        Board board = Mockito.spy(new Board());
-        board.setPlayer(mock(Player.class));
+        Board board = Mockito.spy(new Board(false));
+        board.setPlayer(new Player());
+
+        //Mock de aliens
+        board.setAliens(createMockAliens());
 
         // Reemplazamos `update_shots()` por el método real
         doCallRealMethod().when(board).update_shots();
@@ -52,16 +102,45 @@ public class BoardIntegrationTest {
         verify(board, times(1)).update_shots();
         verify(board, times(1)).update_aliens();
         verify(board, times(1)).update_bomb();
+
+        // Assert: Verificar funcionamiento esperado
+        generalAsserts(board);
+    }
+
+    @Test
+    public void testUpdateWithRealAliens() {
+        // Arrange
+        Board board = Mockito.spy(new Board(false));
+        board.setPlayer(new Player());
+
+        doCallRealMethod().when(board).update_shots();
+        doAnswer(interaction-> {
+            for (Alien alien : board.getAliens()) {alien.act(board.getDirection());}
+            return null;
+        }).when(board).update_aliens();
+        doNothing().when(board).update_bomb();
+
+        // Act
+        board.update();
+
+        // Assert
+        verify(board, times(1)).update_aliens();
+        verify(board, times(1)).update_shots();
+        verify(board, times(1)).update_bomb();
+
+        // Assert: Verificar funcionamiento esperado
+        generalAsserts(board);
     }
 
     @Test
     public void testUpdateWithRealUpdateAliens() {
         // Arrange
-        Board board = Mockito.spy(new Board());
-        board.setPlayer(mock(Player.class));
+        Board board = Mockito.spy(new Board(false));
+        board.setPlayer(new Player());
+        board.setTimer(mock(Timer.class));
 
         // Reemplazar `update_aliens()` por el real
-        doNothing().when(board).update_shots();
+        doCallRealMethod().when(board).update_shots();
         doCallRealMethod().when(board).update_aliens();
         doNothing().when(board).update_bomb();
 
@@ -70,43 +149,45 @@ public class BoardIntegrationTest {
 
         // Assert
         verify(board, times(1)).update_aliens();
-    }
-    @Test
-    public void testUpdateWithRealUpdateBomb() {
-        // Arrange
-        Board board = Mockito.spy(new Board());
-        board.setPlayer(mock(Player.class));
-
-        // Reemplazar `update_bomb()` por el real
-        doNothing().when(board).update_shots();
-        doNothing().when(board).update_aliens();
-        doCallRealMethod().when(board).update_bomb();
-
-        // Act
-        board.update();
-
-        // Assert
+        verify(board, times(1)).update_shots();
         verify(board, times(1)).update_bomb();
+
+        // Assert: Verificar funcionamiento esperado
+        generalAsserts(board);
     }
+
+
     @Test
     public void testUpdateWithAllRealMethods() {
         // Arrange
-        Board board = new Board();
+        Board board = new Board(false);
 
         // Act
         board.update();
 
         // Assert: Verificar que el estado se actualiza correctamente
+        generalAsserts(board);
+    }
+
+    public void generalAsserts(Board board) {
         assertTrue(board.isInGame());
+        assertFalse(board.getShot().isVisible(), "El disparo no debería estar activo");
+        assertFalse(board.getAliens().get(0).isDying(), "El alien no debería estar en estado 'muriendo'");
+        assertFalse(board.getPlayer().isDying(), "El player no debería estar en estado 'muriendo'");
+        assertTrue(board.getPlayer().isVisible(), "El player debería ser visible");
+        assertEquals(board.getPlayer().getX(), Commons.BOARD_WIDTH / 2 - Commons.PLAYER_WIDTH , "El jugador debería estsar en el medio del tablero");
+        assertEquals(board.getPlayer().getY(), Commons.GROUND - Commons.PLAYER_HEIGHT, "El jugador debería estsar en el borde inferior del tablero");
+        assertEquals(board.getAliens().get(0).getX(), Commons.ALIEN_INIT_X - 1, "El alien se debería mover 1 posicion a la izquierda");
     }
 
     private List<Alien> createMockAliens() {
         List<Alien> aliens = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            aliens.add(new Alien(0, 0));
+            Alien alien = mock(Alien.class);
+            when(alien.isVisible()).thenReturn(true);
+            when(alien.getX()).thenReturn(149);
+            aliens.add(alien);
         }
         return aliens;
     }
-
-
 }
